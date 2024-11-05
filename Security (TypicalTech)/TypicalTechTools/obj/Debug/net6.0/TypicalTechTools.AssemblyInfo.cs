78952fdf39c1a3d29dfbb196acea9612f3d1b69d
Benjamin Moore <moorebenjamin30@gmail.com>
2024-11-05 13:08:08 +1000

    push

--- Security (TypicalTech)/TypicalTechTools/obj/Debug/net6.0/TypicalTechTools.AssemblyInfo.cs
+++ Security (TypicalTech)/TypicalTechTools/obj/Debug/net6.0/TypicalTechTools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypicalTechTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d1f9afa8844a6f335e7dc35e2d0b7958a9b14c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0848c8dea0b68bf710c8b507962056e8d56c2f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypicalTechTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypicalTechTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
